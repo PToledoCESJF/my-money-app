e7c806744df91b60bf9a8aa3dc6f813968ab8c2e
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-04-20 19:14:01 -0300

    Frontend antes do Redux

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,7 +1,19 @@
+import '../common/template/dependencies'
 import React from 'react'
+
+import Header from '../common/template/header'
+import SideBar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Routes from './routes'
+import { Route } from 'react-router'
 
 export default props => (
     <div className='wrapper'>
-        <h1>App</h1>
+        <Header />
+        <SideBar />
+        <div className='content-wrapper'>
+            <Routes />
+        </div>
+        <Footer />
     </div>
 )
