1d796396a6661c3af6c5aa2e451b47a286fa0998
Paulo Toledo <ptoledo.bsices@gmail.com>
2020-04-25 17:28:02 -0300

    Utilizando  Link do react-router

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -18,6 +18,7 @@ class BillingCycleForm extends Component {
 
         }
     }
+    
     render(){
         const { handleSubmit, readOnly, credits, debts } = this.props
         const { sumOfCredits, sumOfDebts} = this.calculateSummary()
